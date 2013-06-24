899115ed0f68ac25bdc1f38354fa5975529a6818
Kun Zhou <clirochina@gmail.com>
2013-06-23 20:36:38 -0700

    first BDD

---
+++ features/step_definitions/web_steps.rb
@@ -0,0 +1,24 @@
+Given /^(?:|I )am on (.+)$/ do |page_name|
+	visit path_to(page_name)
+end
+
+When(/^I follow "(.*?)"$/) do |arg1|
+  click_link(arg1)
+end
+
+When(/^I fill in "(.*?)" with "(.*?)"$/) do |field, value|
+  fill_in(field, :with => value)
+end
+
+When(/^I press "(.*?)"$/) do |arg1|
+  click_button(arg1)
+end
+
+Then(/^I should see "(.*?)"$/) do |text|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
+end
+
